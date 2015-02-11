5aa78affc2ee5e8d7571b634a0f715c08bb8de07
irichardson <irichardson@grio.com>
2015-02-10 18:18:05 -0800

    Added static pages for home, about and help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/help'
 
-  resources :users
+  get 'static_pages/about'  
 
-  root 'users#index'
+  # resources :users
+
+  # root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
