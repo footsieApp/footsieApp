dd2a8a28ff9259f1e359a955ce782401b0e19dba
duinn <dunnep6@tcd.ie>
2015-02-22 20:24:37 +0000

    Fnished chap 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :microposts
 
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
