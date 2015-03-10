a3abb2f7321bbbcf2b9c608c68b0c46ba73459f2
duinn <dunnep6@tcd.ie>
2015-03-10 20:47:43 +0000

    Brought in date

--- scrapper scripts/nokogirisample.rb
+++ scrapper scripts/nokogirisample.rb
@@ -2,15 +2,18 @@ require 'nokogiri'
 
 f = File.open("Additional Docs/SportsPages/BBC Test Data.html")
 doc = Nokogiri::HTML(f)
-
-
-matchArray = doc.css('tr.preview') #Get all match instances
-y = 0
-matchArray.each { 
-	|x|
-
-	puts 'Game: ' + y.to_s + " Home: " + x.css('.team-home a').text.strip + ' v Away: ' + x.css('.team-away a').text.strip + " " + x.css('.kickoff').text
-	y += 1
-	}
-
+dateArray = doc.css('table.table-stats')#Date is contain in a super class
+dateArray.each {
+	|dateElement|
+	matchArray = dateElement.css('tr.preview')#Get all the match instances for the date
+	y = 0
+	matchArray.each { 
+		|x|
+		puts dateElement.css('caption').text[38..-1] + " " + x.css('.kickoff').text.strip
+		puts 'Game: ' + y.to_s + " Home: " + x.css('.team-home a').text.strip + ' v Away: ' + x.css('.team-away a').text.strip
+		y += 1
+		}
+}
 f.close
+
+
