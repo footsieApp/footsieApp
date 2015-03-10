2b385e29cd774906be82496aa63a1a40faf5b190
duinn <dunnep6@tcd.ie>
2015-03-10 20:12:20 +0000

    Nokogiri exmaple

---
+++ scrapper scripts/nokogirisample.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+
+f = File.open("//home/duinn/Woerkspace/master/footsieApp/Additional Docs/SportsPages/BBC Test Data.html")
+doc = Nokogiri::HTML(f)
+
+
+matchArray = doc.css('tr.preview') #Get all match instances
+y = 0
+matchArray.each { 
+	|x|
+
+	puts 'Game: ' + y.to_s + " Home: " + x.css('.team-home a').text.strip + ' v Away: ' + x.css('.team-away a').text.strip + " " + x.css('.kickoff').text
+	y += 1
+	}
+
+f.close
