c45a5a6344997b8bbea93e77d7e26045875eb492
duinn <dunnep6@tcd.ie>
2015-03-25 20:29:48 +0000

    added big stupid button to launch scrapper

--- app/controllers/scrapper_histories_controller.rb
+++ app/controllers/scrapper_histories_controller.rb
@@ -1,6 +1,24 @@
 class ScrapperHistoriesController < ApplicationController
   before_action :set_scrapper_history, only: [:show, :edit, :update, :destroy]
 
+  def scrape_local
+    f = File.open(Dir.pwd+"/Additional Docs/SportsPages/BBC Test Data.html")
+    doc = Nokogiri::HTML(f)
+    dateArray = doc.css('table.table-stats')#Date is contain in a super class
+    dateArray.each {
+      |dateElement|
+      matchArray = dateElement.css('tr.preview')#Get all the match instances for the date
+      y = 0
+      matchArray.each { 
+        |x|
+        puts dateElement.css('caption').text[38..-1] + " " + x.css('.kickoff').text.strip
+        puts 'Game: ' + y.to_s + " Home: " + x.css('.team-home a').text.strip + ' v Away: ' + x.css('.team-away a').text.strip
+        y += 1
+        }
+    }
+    f.close
+    redirect_to :back
+  end
   # GET /scrapper_histories
   # GET /scrapper_histories.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,18 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'help'    => 'static_pages#help'
 
   get 'static_pages/about'
+
+  get 'static_pages/buttonlauncher'
+
+  get 'scrape_local' => 'scrapper_histories#scrape_local'
 
   resources :microposts
 
   resources :users
-  root 'static_pages#home'
+  root 'static_pages#buttonlauncher'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
